4dfa37c76eb29130e3bdbc29556ef8a23b1056cb
lukeludwig <luke.ludwig@tstmedia.com>
2012-03-04 16:03:39 -0600

    README, comments, and ENV variable for log threshold

--- lib/rack_timer/stack.rb
+++ lib/rack_timer/stack.rb
@@ -1,11 +1,11 @@
 module ActionDispatch
   class MiddlewareStack < Array
 
-    # this will wrap around each Rack-based middleware and take timing snapshots of how long
+    # this class will wrap around each Rack-based middleware and take timing snapshots of how long
     # each middleware takes to execute
     class RackTimer
 
-      LogThreshold = 1.0 # millisecond
+      LogThreshold = ENV['RACK_TIMER_LOG_THRESHOLD'].to_f || 1.0 # millisecond
 
       def initialize(app)
         @app = app
@@ -13,14 +13,14 @@ module ActionDispatch
 
       def call(env)
         if env.has_key?("MIDDLEWARE_TIMESTAMP") # skip over the first middleware
-          time_taken = (Time.now.to_f - env["MIDDLEWARE_TIMESTAMP"][1].to_f) * 1000 
-          if time_taken > LogThreshold # only log if took greater than LogThreshold
-            Rails.logger.info "Rack Timer -- #{env["MIDDLEWARE_TIMESTAMP"][0]}: #{time_taken} ms"
+          elapsed_time = (Time.now.to_f - env["MIDDLEWARE_TIMESTAMP"][1].to_f) * 1000 
+          if elapsed_time > LogThreshold # only log if took greater than LogThreshold
+            Rails.logger.info "Rack Timer -- #{env["MIDDLEWARE_TIMESTAMP"][0]}: #{elapsed_time} ms"
           end
         elsif env.has_key?("HTTP_X_REQUEST_START")
-          # if we are tracking request queuing time then lets see how much time was spent in the request
-          # queue by taking the difference between Time.now from the start of the first piece of middleware
-          Rails.logger.info "Rack Timer -- #{env['HTTP_X_REQUEST_START']}"
+          # if we are tracking request queuing time via New Relic's suggested header,
+          # then lets see how much time was spent in the request queue by taking the difference
+          # between Time.now from the start of the first piece of middleware
           queue_start_time = env["HTTP_X_REQUEST_START"].gsub("t=", "").to_i
           Rails.logger.info "Rack Timer -- Queuing time: #{(Time.now.to_f * 1000000).to_i - queue_start_time} microseconds"
         end
